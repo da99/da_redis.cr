b716d63197e45168974165135f60e5b4e791831f
da99 <i-hate-spam-1234567@mailinator.com>
2018-02-19 16:00:42 -0600

    Dev. checkpoint.

--- specs/specs.cr
+++ specs/specs.cr
@@ -1,6 +1,9 @@
 
 require "../src/da_redis"
 
+# This is a place holder.
+# I will write better tests later on.
+
 port = 6380
 conn = DA_Redis.new(port)
 puts conn.send("PING")
@@ -10,3 +13,4 @@ puts conn.send("SET", "my_temp.date", Time.now.to_s, "EX", 5.to_s)
 puts conn.send("GET", "my_temp.value")
 puts conn.send("ECHO", "hello world")
 puts conn.keys("my_temp.*")
+conn.close
